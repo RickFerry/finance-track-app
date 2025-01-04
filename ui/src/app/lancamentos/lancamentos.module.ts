e689775af7e3ede214c7a90a31c813b4c176d543
RickFerry <rick_ferry1@hotmail.com>
2025-01-04 11:45:58 -0300

    Novo modulo de lancamentos

--- ui/src/app/lancamentos/lancamentos.module.ts
+++ ui/src/app/lancamentos/lancamentos.module.ts
@@ -1,12 +1,11 @@
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
@@ -16,24 +15,12 @@ import { SelectButtonModule } from 'primeng/selectbutton';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    PessoasPesquisaComponent,
-    PessoaCadastroComponent,
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LancamentosModule,
+    CommonModule,
     InputTextModule,
     ButtonModule,
     TableModule,
@@ -45,13 +32,11 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
     SelectButtonModule,
     DropdownModule,
     InputNumberModule,
-    InputMaskModule,
     FormsModule,
     MessagesModule,
     MessageModule,
   ],
-
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [LancamentoCadastroComponent, LancamentosPesquisaComponent],
+  exports: [LancamentoCadastroComponent, LancamentosPesquisaComponent],
 })
-export class AppModule {}
+export class LancamentosModule {}
