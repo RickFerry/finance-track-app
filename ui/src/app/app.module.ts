61e4ad4c696970bbe1a8462b89dffc0ca3d0c34d
RickFerry <rick_ferry1@hotmail.com>
2025-01-04 12:06:03 -0300

    Refatorando app.module

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,20 +1,5 @@
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { MessageModule } from 'primeng/message';
-import { MessagesModule } from 'primeng/messages';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,27 +9,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LancamentosModule,
-    PessoasModule,
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    ScrollingModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule,
-    BrowserAnimationsModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputNumberModule,
-    InputMaskModule,
-    FormsModule,
-    MessagesModule,
-    MessageModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, LancamentosModule, PessoasModule],
 
   providers: [],
   bootstrap: [AppComponent],
