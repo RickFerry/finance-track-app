4e292b56638f9dea0def1e3f1272da2087177553
RickFerry <rick_ferry1@hotmail.com>
2025-01-15 23:28:10 -0300

    Ajuste

--- ui/src/app/lancamentos/Lancamento.service.ts
+++ ui/src/app/lancamentos/Lancamento.service.ts
@@ -9,15 +9,12 @@ export class LancamentoService {
 
   constructor(private http: HttpClient) {}
 
-  pesquisar(): Promise<any> {
-    const headers = new HttpHeaders();
-    headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
+  async pesquisar(): Promise<any> {
+    const headers = new HttpHeaders({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzY5OTQ3ODQsInVzZXJfbmFtZSI6ImFkbWluQGVtYWlsLmNvbSIsImF1dGhvcml0aWVzIjpbIlJPTEVfQ0FEQVNUUkFSX0NBVEVHT1JJQSIsIlJPTEVfUEVTUVVJU0FSX1BFU1NPQSIsIlJPTEVfUkVNT1ZFUl9QRVNTT0EiLCJST0xFX0NBREFTVFJBUl9MQU5DQU1FTlRPIiwiUk9MRV9QRVNRVUlTQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUkVNT1ZFUl9MQU5DQU1FTlRPIiwiUk9MRV9DQURBU1RSQVJfUEVTU09BIiwiUk9MRV9QRVNRVUlTQVJfQ0FURUdPUklBIiwiUk9MRV9SRU1PVkVSX0NBVEVHT1JJQSJdLCJqdGkiOiIzNjA1NGE0Ny00Mzg1LTRlNTgtYmY0Zi04NzY0ODQ4MDQxNzEiLCJjbGllbnRfaWQiOiJhbmd1bGFyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl19.4nI_mnBfMku2WQYo9smF1rQvNOjFsb5VFlo6-udxZGk'});
 
-    return this.http
+    const response = await this.http
       .get(`${this.lancamentoUrl}?resumo`, { headers })
-      .toPromise()
-      .then((response) => {
-        console.log(JSON.stringify(response));
-      });
+      .toPromise();
+    console.log(JSON.stringify(response));
   }
 }
