3de8346165440ace230e156a87cde9e610b718f8
Ricardo <rick_ferry1@hotmail.com>
2025-01-17 22:29:37 -0300

    Exclusao de pessoa com toast e confirmacao

--- ui/src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ ui/src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { PessoaFiltro, PessoaService } from '../pessoa.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -12,13 +14,33 @@ export class PessoasPesquisaComponent {
   filtro = new PessoaFiltro();
   pessoas = [];
 
-  constructor(private service: PessoaService) {}
+  constructor(
+    private service: PessoaService,
+    private msgService: MessageService,
+    private confirm: ConfirmationService,
+    private errorHandler: ErrorHandlerService
+  ) {}
 
   pesquisar(pagina = 0): void {
     this.filtro.pagina = pagina;
     this.service.pesquisar(this.filtro).then((pess) => {
       this.totalRegistros = pess.totalElements;
       this.pessoas = pess.content;
+    });
+  }
+
+  excluir(Pessoa: any) {
+    this.confirm.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.service.excluir(Pessoa.codigo).then(() => {
+          this.msgService.add({
+            severity: 'success',
+            summary: 'Pessoa excluída com sucesso!',
+          });
+          this.pesquisar();
+        });
+      },
     });
   }
 
