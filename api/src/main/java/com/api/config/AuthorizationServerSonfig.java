192869aba83d4d3f402f4dda6ef2deb0d4b811fc
RickFerry <rick_ferry1@hotmail.com>
2025-01-25 16:11:27 -0300

    Configurando ambiente de prod

--- api/src/main/java/com/api/config/AuthorizationServerSonfig.java
+++ api/src/main/java/com/api/config/AuthorizationServerSonfig.java
@@ -1,7 +1,6 @@
 package com.api.config;
 
 import com.api.token.CustomTokenEnhancer;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
