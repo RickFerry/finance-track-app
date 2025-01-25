7698d4a3b61e9b6f8d4a7a4a14154cd1fa517f47
RickFerry <rick_ferry1@hotmail.com>
2025-01-25 16:00:13 -0300

    Adicionando dois perfis de seguranca

--- api/src/main/java/com/api/config/AuthorizationServerSonfig.java
+++ api/src/main/java/com/api/config/AuthorizationServerSonfig.java
@@ -2,8 +2,10 @@ package com.api.config;
 
 import com.api.token.CustomTokenEnhancer;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -18,11 +20,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import java.util.Arrays;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
+@Profile("oauth-security")
 @EnableAuthorizationServer
 public class AuthorizationServerSonfig extends AuthorizationServerConfigurerAdapter {
 
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
